d458bafb28a05e60191e6b98a3933caacd29776c
Munish Jalota <mjalota@gmail.com>
2025-06-12 16:35:54 +0100

    update api version (#35)

--- src/test/java/uk/gov/hmcts/cp/controllers/CourtScheduleControllerTest.java
+++ src/test/java/uk/gov/hmcts/cp/controllers/CourtScheduleControllerTest.java
@@ -7,10 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.server.ResponseStatusException;
+import uk.gov.hmcts.cp.openapi.model.CourtSchedule;
 import uk.gov.hmcts.cp.openapi.model.CourtScheduleResponse;
-import uk.gov.hmcts.cp.openapi.model.CourtScheduleResponseCourtScheduleInner;
-import uk.gov.hmcts.cp.openapi.model.CourtScheduleResponseCourtScheduleInnerHearingsInner;
-import uk.gov.hmcts.cp.openapi.model.CourtScheduleResponseCourtScheduleInnerHearingsInnerCourtSittingsInner;
+import uk.gov.hmcts.cp.openapi.model.CourtSitting;
+import uk.gov.hmcts.cp.openapi.model.Hearing;
 import uk.gov.hmcts.cp.repositories.CourtScheduleRepository;
 import uk.gov.hmcts.cp.repositories.InMemoryCourtScheduleRepositoryImpl;
 import uk.gov.hmcts.cp.services.CourtScheduleService;
@@ -52,11 +52,11 @@ class CourtScheduleControllerTest {
         assertNotNull(responseBody.getCourtSchedule());
         assertEquals(1, responseBody.getCourtSchedule().size());
 
-        CourtScheduleResponseCourtScheduleInner schedule = responseBody.getCourtSchedule().get(0);
+        CourtSchedule schedule = responseBody.getCourtSchedule().get(0);
         assertNotNull(schedule.getHearings());
         assertEquals(1, schedule.getHearings().size());
 
-        CourtScheduleResponseCourtScheduleInnerHearingsInner hearing = schedule.getHearings().get(0);
+        Hearing hearing = schedule.getHearings().get(0);
         assertNotNull(hearing.getHearingId());
         assertEquals("Requires interpreter", hearing.getListNote());
         assertEquals("Sentencing for theft case", hearing.getHearingDescription());
@@ -64,7 +64,7 @@ class CourtScheduleControllerTest {
         assertNotNull(hearing.getCourtSittings());
         assertEquals(1, hearing.getCourtSittings().size());
 
-        CourtScheduleResponseCourtScheduleInnerHearingsInnerCourtSittingsInner sitting =
+        CourtSitting sitting =
                 hearing.getCourtSittings().get(0);
         assertEquals("Central Criminal Court", sitting.getCourtHouse());
         assertNotNull(sitting.getSittingStart());
