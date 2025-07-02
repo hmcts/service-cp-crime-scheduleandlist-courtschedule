d354985dabe6f653a4c0978a01904f92e3efefd6
Vipin Palengara <61233682+meetvipinp@users.noreply.github.com>
2025-07-02 12:28:36 +0100

    update package + added json to object mapper (#47)

--- src/pactVerificationTest/java/uk/gov/hmcts/cp/pact/provider/CourtScheduleProviderPactTest.java
+++ src/pactVerificationTest/java/uk/gov/hmcts/cp/pact/provider/CourtScheduleProviderPactTest.java
@@ -16,14 +16,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.cp.openapi.model.CourtScheduleResponse;
-import uk.gov.hmcts.cp.openapi.model.CourtSchedule;
-import uk.gov.hmcts.cp.openapi.model.Hearing;
-import uk.gov.hmcts.cp.openapi.model.CourtSitting;
+import uk.gov.hmcts.cp.pact.helper.JsonFileToObject;
 import uk.gov.hmcts.cp.repositories.CourtScheduleRepository;
-
-import java.time.OffsetDateTime;
-import java.util.List;
-import java.util.UUID;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith({SpringExtension.class, PactVerificationInvocationContextProvider.class})
@@ -50,31 +44,10 @@ public class CourtScheduleProviderPactTest {
     }
 
     @State("court schedule for case 456789 exists")
-    public void setupCourtSchedule() {
+    public void setupCourtSchedule() throws Exception{
         courtScheduleRepository.clearAll();
-        var courtSitting = CourtSitting.builder()
-                .courtHouse("Central Criminal Court")
-                .sittingStart(OffsetDateTime.now())
-                .sittingEnd(OffsetDateTime.now().plusMinutes(60))
-                .judiciaryId(UUID.randomUUID().toString())
-                .build();
-        var hearing = Hearing.builder()
-                .hearingId(UUID.randomUUID().toString())
-                .listNote("Requires interpreter")
-                .hearingDescription("Sentencing for theft case")
-                .hearingType("Trial")
-                .courtSittings(List.of(courtSitting))
-                .build();
-
-        var schedule = CourtSchedule.builder()
-                .hearings(List.of(hearing))
-                .build();
-
-        var response = CourtScheduleResponse.builder()
-                .courtSchedule(List.of(schedule))
-                .build();
-
-        courtScheduleRepository.saveCourtSchedule("456789", response);
+        CourtScheduleResponse courtScheduleResponse = JsonFileToObject.readJsonFromResources("courtSchedule.json", CourtScheduleResponse.class);
+        courtScheduleRepository.saveCourtSchedule("456789", courtScheduleResponse);
     }
 
     @TestTemplate
