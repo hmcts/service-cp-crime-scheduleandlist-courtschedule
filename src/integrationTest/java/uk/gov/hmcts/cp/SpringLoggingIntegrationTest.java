285033cbc5fadefbe7fda57af630aa7c36256562
madhusudan.yasa <madhusudan.yasa@hmcts.net>
2026-01-12 14:17:46 +0000

    AMP-192 - addressed review comments

--- src/integrationTest/java/uk/gov/hmcts/cp/SpringLoggingIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/cp/SpringLoggingIntegrationTest.java
@@ -1,16 +1,20 @@
 package uk.gov.hmcts.cp;
 
+import ch.qos.logback.classic.AsyncAppender;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import uk.gov.hmcts.cp.config.IntegrationTestConfig;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,12 +36,17 @@ class SpringLoggingIntegrationTest {
         MDC.put("any-mdc-field", "1234-1234");
         final ByteArrayOutputStream capturedStdOut = captureStdOut();
         log.info("spring boot test message");
-        
+
+        AsyncAppender asyncAppender = (AsyncAppender) ((ch.qos.logback.classic.Logger) LoggerFactory
+                .getLogger("ROOT"))
+                .getAppender("ASYNC_JSON");
+        asyncAppender.stop();
+
         Map<String, Object> capturedFields = new ObjectMapper().readValue(capturedStdOut.toString(), new TypeReference<>() {
         });
 
         assertThat(capturedFields.get("any-mdc-field")).isEqualTo("1234-1234");
-        assertThat(capturedFields.get("message")).isEqualTo("spring boot test message");
+        assertThat((String) capturedFields.get("message")).contains("spring boot test message");
         assertThat(capturedFields.get("logger_name")).isEqualTo(this.getClass().getName());
         assertThat(capturedFields.get("thread_name")).isNotNull();
         assertThat(capturedFields.get("level")).isEqualTo("INFO");
